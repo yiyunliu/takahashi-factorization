b8601c317c87cc76c5f188ac1d90bf37052c98fb
Yiyun Liu <liuyiyun@seas.upenn.edu>
2024-06-21 16:44:51 -0400

    Prove local postponement for leftmost-outermost reduction

--- theories/normalization.v
+++ theories/normalization.v
@@ -218,33 +218,10 @@ Proof.
   - hauto lqb:on.
 Qed.
 
-(* Lemma starseq_app_cong0 M N P Q : *)
-(*   M ⇒n N -> *)
-(*   P ⇒ Q -> *)
-(*   starseq (tApp M P) (tApp N Q). *)
-(* Proof. *)
-(*   move => h. move : P Q. elim : M N / h. *)
-(*   - move => M N hMN P Q hPQ. *)
-(*     apply S_Refl. *)
-
-(* Lemma starseq_app_cong M N P Q : *)
-(*   starseq M N -> *)
-(*   P ⇒n Q -> *)
-(*   starseq (tApp M P) (tApp N Q). *)
-(* Proof. *)
-(*   move => h. move : P Q. elim : M N / h. *)
-(*   - move => M N hMN P Q hPQ. *)
-(*     best. *)
-
-(*   - move => M P N hMP hMN hPN ih R Q hRQ. *)
-(*     case E : (isAbs M). *)
-(*     + apply S_Refl. *)
-(*       case : M hMP hMN E=>//. *)
-(*       move => M. *)
-(*       elim/ered_inv=>//_ a0 b0 hab0 [?] ? h _. subst. *)
-(*       hauto lq:on inv:Par ctrs:NPar. *)
-(*     + hauto lq:on ctrs:starseq, NPar, Par, ERed. *)
-(* Qed. *)
+Lemma ne_red_imp a b : a ⤳h b  -> (ne a || nf a) -> False.
+Proof.
+  move => h. elim : a b /h; hauto lqb:on.
+Qed.
 
 Lemma starseq_app_cong1 a b0 b1 :
   ne a ->
@@ -281,8 +258,6 @@ Proof.
   elim : a b /h; sfirstorder use:NPar_Par.
 Qed.
 
-
-
 Lemma starseq_app_cong a0 a1 b0 b1 :
   starseq a0 a1 ->
   starseq b0 b1 ->
@@ -313,7 +288,6 @@ Lemma starseq_abs_cong M N
   starseq (tAbs M) (tAbs N).
 Proof. elim:M N/h; hauto lq:on ctrs:starseq, NPar, Par, ERed. Qed.
 
-
 Lemma starseq_ρ_par ρ0 ρ1 :
   (forall i : fin, starseq (ρ0 i) (ρ1 i)) ->
   (forall i : fin, Par (ρ0 i) (ρ1 i)).
@@ -330,7 +304,6 @@ Proof.
   - move => a0 a1 b0 b1 ha iha hb ihb ρ0 ρ1 hρ /=.
     apply starseq_app_cong.
     sfirstorder.
-    (* par cong *)
     sfirstorder use:Par_morphing, starseq_ρ_par.
   - move => a0 a1 b0 b1 ha iha hb ihb ρ0 ρ1 h.
     simpl.
@@ -338,9 +311,7 @@ Proof.
     by apply ER_AppAbs.
     apply P_AppAbs' with (a1 := subst_tm (up_tm_tm ρ1) a1) (b1 := subst_tm ρ1 b1).
     by asimpl.
-    (* par cong *)
     sfirstorder use:Par_morphing, starseq_ρ_par, morphing_suc.
-    (* par cong *)
     sfirstorder use:Par_morphing, starseq_ρ_par.
     asimpl.
     apply iha.
@@ -369,6 +340,47 @@ Proof.
   - eauto using starseq_abs_cong.
 Qed.
 
+Lemma not_ne_red a : ~~ ne a -> ~~  nf a -> exists b, a ⤳h b.
+Proof.
+  elim : a => //=.
+  - hauto lq:on inv:tm lq:on ctrs:ERed b:on.
+  - move => M ihM N ihN h _.
+    case E : (ne M) => //=.
+    + hauto q:on inv:tm ctrs:ERed.
+    + move {h}.
+      case : (nf M)=> //=;
+      hauto lq:on inv:tm ctrs:ERed.
+Qed.
+
+Lemma ne_characterization a : ~~ (isAbs a) -> nf a -> ne a.
+Proof. elim : a => //=.
+Qed.
+
+Lemma ne_characterization' a :  ~~ ne a -> isAbs a \/ ~~ nf a.
+Proof.
+  hauto lq:on inv:tm use:ne_characterization.
+Qed.
+
+Lemma persistence t s1 s2 (h : t ⤳h s1) :
+  t ⇒n s2 ->
+  exists u, s2 ⤳h u.
+Proof.
+  move : s2.
+  elim : t s1 /h.
+  - move => a0 a1 b h0 h1 ih0 s2.
+    inversion 1; subst.
+    + scongruence.
+    + move : ih0 (H3) => /[apply].
+      move => [u]hu.
+      case E : (isAbs a3) => //=.
+      * hauto lq:on ctrs:ERed inv:tm.
+      * hauto lq:on ctrs:ERed.
+    + hauto lq:on ctrs:ERed.
+  - hauto lqb:on inv:NPar ctrs:ERed.
+  - hauto lqb:on inv:NPar ctrs:ERed.
+  - hauto lqb:on inv:NPar ctrs:ERed.
+Qed.
+
 Lemma hms_merge t a u  :
   t ⇒n a ->
   a ⤳h u ->
@@ -380,13 +392,28 @@ Proof.
   - hauto lq:on ctrs:Par inv:ERed.
   - hauto q:on ctrs:Par inv:ERed.
   - hauto lq:on ctrs:Par inv:ERed.
-  - hauto lq:on inv:NPar, ERed ctrs:Par use:NPar_Par.
+  - move => a0 a1 b0 b1 nea0 ha iha hb u.
+    elim /ered_inv => //= _.
+    + hauto lq:on ctrs:Par.
+    + move => ? ? b2 nea2 h [*]. subst.
+      move /ne_characterization' : (nea0) => [].
+      * case : a0 nea0 ha iha => //=.
+        hauto lq:on inv:NPar ctrs:Par.
+      * move => nfa0.
+        have [a2 ha2] : exists a2, a0 ⤳h a2 by sfirstorder use:not_ne_red.
+        exfalso. move : ha2 ha nea2; clear.
+        move : persistence; repeat move/[apply].
+        sfirstorder lqb:on use:ne_red_imp.
+    + move => a2 b2 [*]. subst.
+      hauto lq:on inv:NPar ctrs:Par use:NPar_Par.
+  - move => a b0 b1 ha hb ihb u.
+    inversion 1; subst => //.
+    hauto lqb:on use:ne_red_imp.
+    hauto lq:on ctrs:Par use:Par_refl, ERed_Par.
 Qed.
-
-
 
 Lemma local_postponement t a u  :
   t ⇒n a ->
   a ⤳h u ->
-  exists q, t ⤳*h q /\ q ⇒n u.
+  exists q, rtc ERed t q /\ q ⇒n u.
 Proof. sfirstorder use:hms_split, hms_merge, starseq_erase. Qed.
